aa20621799ed8a46c6c6bf0a25ae1c5f3ec08d35
Ramki A <kvraamkeydev@gmail.com>
2023-08-04 00:20:50 +0530

    fix: ghpage publish issue fixed

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
     plugins: [react(), splitVendorChunkPlugin()],
+    base: '/react-ui-library/',
     resolve: {
         alias: {
             '~': resolve(__dirname, './src'),
