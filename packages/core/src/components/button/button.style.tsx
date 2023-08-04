75f9aa89f14cba857cce719575b372e9f1958a77
Ramki A <kvraamkeydev@gmail.com>
2023-08-04 20:40:45 +0530

    Button component changes

--- packages/core/src/components/button/button.style.tsx
+++ packages/core/src/components/button/button.style.tsx
@@ -1,10 +1,10 @@
 import { tv } from 'tailwind-variants';
 
 export const StyledButton = tv({
-    base: 'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+    base: 'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50',
     variants: {
         variant: {
-            default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+            default: 'bg-default text-default-foreground shadow hover:bg-default/90',
             destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
             outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
             secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
