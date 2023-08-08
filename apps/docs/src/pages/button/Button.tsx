5ca656ad76f55316b1cbb9fc47fd4dcbc79b0669
Ramki A <kvraamkeydev@gmail.com>
2023-08-08 23:39:52 +0530

    components moved to pages folder

---
+++ apps/docs/src/pages/button/Button.tsx
@@ -0,0 +1,21 @@
+import { Description, Playground, Preview, PreviewWrapper, Properties } from "~/components"
+
+export default () => {
+    return (
+        <>
+            <PreviewWrapper title="Button">
+
+                <Preview />
+
+                <Description>
+                </Description>
+
+                <Properties />
+
+            </PreviewWrapper>
+
+            <Playground />
+        </>
+
+    )
+}
