d717ed3d1ce1771b97d71f69b845895ce1d91da4
Ramki A <kvraamkeydev@gmail.com>
2023-08-11 19:22:05 +0530

    button component doc updated

--- apps/docs/src/pages/button/Button.tsx
+++ apps/docs/src/pages/button/Button.tsx
@@ -1,49 +1,72 @@
 import React from "react";
 import { Button, ButtonProps } from "@kvraamkey/core"
 
-import props from "./props"
+import { usageProps, playgroundProps } from "~/pages/button/props"
+import { codeTemplate } from "~/pages/button/code";
 import { Description, Playground, Preview, PreviewWrapper, Properties } from "~/components"
-
-const codeTemplate = (props: string, children: string) => `
-import { Button } from '@mantine/core';
-
-function Demo() {
-  return (
-    <Button${props}>
-      ${children}
-    </Button>
-  );
-}
-`;
 
 export default () => {
 
-    const [state, setState] = React.useState<ButtonProps>({ variant: 'default', children: 'Button', size: 'lg' });
+    const [state, setState] = React.useState<ButtonProps>({ children: 'Contained' });
 
     const setStateField = (field: string, value: any) => {
         setState((current) => ({ ...current, [field]: value }));
     }
 
-
-    const code = codeTemplate("", (state as any).children);
-
     return (
         <>
-            <PreviewWrapper title="Button">
+            <PreviewWrapper title="Button" desc="Buttons allow users to take actions, and make choices, with a single tap.">
 
-                <Preview code={code}>
-                    <Button {...state} />
+                <Preview code={codeTemplate(state, (state as any).children)}>
+                    <Button {...state} style={{ transition: 'all .5s ease' }} />
                 </Preview>
 
                 <Description>
                     <p>Button component contains two major types of buttons, namely, regular (default) and icon button (when setting icon property). Icon button can be configured as FAB (Floating Action Button) by setting fab property. The button can become a toggle by setting the toggle property</p>
                 </Description>
 
-                <Properties title="Button component props" props={props} />
+                <Properties title="Button component props" props={usageProps} />
 
             </PreviewWrapper>
 
-            <Playground />
+            <Playground>
+                <pre>{JSON.stringify(state, null, 2)}</pre>
+                <div className="col-span-full">
+                    <label htmlFor="children" className="block text-sm font-medium leading-6 text-gray-900 capitalize">Children</label>
+                    <input name="children" value={state?.children} id="children" onChange={(e) => setStateField('children', e.target.value)} />
+                </div>
+                <div className="col-span-full">
+                    <label htmlFor="color" className="block text-sm font-medium leading-6 text-gray-900 capitalize">color</label>
+                    <select name="color" id="color" onChange={(e) => setStateField('color', e.target.value)}>
+                        <option value="primary">Text</option>
+                        <option value="default">Filled</option>
+                        <option value="secondary">Outlined</option>
+                    </select>
+                </div>
+                <div className="col-span-full">
+                    <label htmlFor="variant" className="block text-sm font-medium leading-6 text-gray-900 capitalize">variant</label>
+                    <select name="variant" id="variant" onChange={(e) => setStateField('variant', e.target.value)}>
+                        <option value="primary">Text</option>
+                        <option value="default">Filled</option>
+                        <option value="secondary">Outlined</option>
+                    </select>
+                </div>
+                <div className="col-span-full">
+                    <label htmlFor="radius" className="block text-sm font-medium leading-6 text-gray-900 capitalize">radius</label>
+                    <select name="radius" id="radius" onChange={(e) => setStateField('radius', e.target.value)}>
+                        <option value="small">small</option>
+                        <option value="medium">medium</option>
+                        <option value="large">large</option>
+                        <option value="full">full</option>
+                    </select>
+                </div>
+
+                <div className="col-span-full">
+                    <label htmlFor="className" className="block text-sm font-medium leading-6 text-gray-900 capitalize">className</label>
+                    <input name="className" id="className" onChange={(e) => setStateField('className', e.target.value)} />
+                </div>
+
+            </Playground>
         </>
 
     )
--- apps/docs/src/pages/button/props.ts
+++ apps/docs/src/pages/button/props.ts
@@ -1,4 +1,4 @@
-export default [
+export const usageProps = [
     {
         name: 'name',
         desc: 'Name of the button',
@@ -53,4 +53,27 @@ export default [
         type: 'string',
         def: '',
     },
+];
+
+export const playgroundProps = [
+    {
+        name: 'variant',
+        type: 'select',
+        data: [
+            { label: 'filled', value: 'filled' },
+            { label: 'light', value: 'light' },
+            { label: 'outline', value: 'outline' },
+            { label: 'default', value: 'default' },
+            { label: 'subtle', value: 'subtle' },
+        ],
+        initialValue: 'filled',
+        defaultValue: 'filled',
+    },
+    { name: 'color', type: 'color', initialValue: 'blue', defaultValue: 'blue' },
+    { name: 'radius', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+    { name: 'size', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+    { name: 'disabled', type: 'boolean', initialValue: false, defaultValue: false },
+    { name: 'compact', type: 'boolean', initialValue: false, defaultValue: false },
+    { name: 'uppercase', type: 'boolean', initialValue: false, defaultValue: false },
+    { name: 'children', type: 'string', initialValue: 'Settings' },
 ];
